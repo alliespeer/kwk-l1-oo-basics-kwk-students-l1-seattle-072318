574594cb043ca3f5c0ea00ba43519326921a64c1
alliespeer <alliespeer13@gmail.com>
2018-07-27 16:20:04 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
 # book.rb
-# Add you Book class here
+class Book 
+  attr_accessor :suzanne, :365, :fiction
+  attr_reader : the_hunger_games
+end
+  def initialize(the_hunger_games)
+@the_hunger_games = the_hunger_games
+end 
+
+def the_hunger_games
+  @the_hunger_games
+end
+
+def author = (suzanne)
+  @suzanne = suzanne
+ end
+ 
+ def suzanne
+   @suzanne
+ end
+ 
+ def page_count = (365)
+   @page_count = (365)
+end
+
+def genre = ( genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+
+def turn_page
+  puts "wow youre reaing this book so fast"
+  
+  
